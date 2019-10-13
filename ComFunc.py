304bc53f9d5d58f1244d8685f3cec357a65d0d31
AlecMcG99 <53317718+AlecMcG99@users.noreply.github.com>
2019-10-13 15:17:11 -0500

    Update ComFunc.py

--- ComFunc.py
+++ ComFunc.py
@@ -1,15 +1,14 @@
 '''
 TODO:
 A function that translates DNA to Amino Acidse
-File into gb
 Find coordinates of open reading frames
-Finds exons
 
 DONE:
 parse fasta
 parse gb
 File into fasta
 find CDS list
+Finds exons
 '''
 import re
 
@@ -48,8 +47,9 @@ def parse_gb(FileName):
 		DNA_seq += ''.join(lst[1:])
 	return DNA_seq.upper()
 
-def new_fasta(newFileName, body, heading = '>CFTR coding \n'):
+def new_fasta(newFileName, body, heading):
 	newfile = open(newFileName, 'w+')
+	heading += '\n'
 	newfile.write(heading)
 	newfile.write(body)
 	newfile.close()
@@ -83,7 +83,7 @@ def find_exons(FileName, list = True):
 
 	exon_sequence = ""
 	exon_sequence_list = []
-	DNA_seq = parse_gb(File)
+	DNA_seq = parse_gb(FileName)
 	# now pull it out
 	for index in range(0,len(CDS_list),2):
 	    begin = int(CDS_list[index]) - 1
@@ -120,3 +120,32 @@ def find_introns(FileName, list = True):
 		return intron_list
 	else:
 		return intron_seq
+
+def translate(DNA):
+	genecode = {
+	    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+	    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+	    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+	    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+	    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+	    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+	    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+	    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+	    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+	    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+	    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+	    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+	    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+	    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+	    'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+	    'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W'}
+
+	prot_seq = ''
+	for i in range(0,len(DNA), 3):
+		codon = DNA[i:i+3]
+		AA = genecode[codon]
+		prot_seq += AA
+
+	return prot_seq
+
+
